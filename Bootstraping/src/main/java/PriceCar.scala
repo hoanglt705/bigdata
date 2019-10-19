46dc81f33cbf7780e7cb804a608359d7643635c1
hoanglt705 <hoanglt705@gmail.com>
2019-10-19 00:42:44 -0500

    Change code

--- Bootstraping/src/main/java/PriceCar.scala
+++ Bootstraping/src/main/java/PriceCar.scala
@@ -9,11 +9,9 @@ import org.apache.spark.sql.{Row, SparkSession}
 
 case class CarType(carType: String, price: Double)
 object CarPrice {
-
-  def FILENAME = "carprice"
   def K = "carType"
   def V = "price"
-  def TIMES = 3
+  def TIMES = 10
   def SAMPLE_RATE = 0.75
 
   def main(args: Array[String]): Unit = {
@@ -30,33 +28,31 @@ object CarPrice {
     println("DATA ROWS: " + mtcdata.count())
     
     val carTypes = mtcdata.map(p => CarType(p(0), p(2).toDouble)).toDF.cache()
-    
-    carTypes.printSchema
         
-    carTypes.select(K,V).show(100)
+    carTypes.select(K,V).show(48)
     
     // Aggregate data after grouping by columns
     import org.apache.spark.sql.functions._
     println("Step 2: Select a categorical variable and a numeric variable and form the key-value pair and create a pairRDD called “population”")
     val population = carTypes.select(K,V).cache()
-    population.show(100)
+    population.show()
 
     println("Step 3. Compute the mean price and variance for each car type and display")
     val mean_variance  = population.groupBy(K).agg(avg(V) as "Mean",variance(V) as "Variance").show()
 
     println("Step 4: Create the sample for bootstrapping, take 25% of the population without replacement.")
     val sampleRdd = population.sample(false,SAMPLE_RATE)
-    sampleRdd.show(100)
+    sampleRdd.show()
 
-    println("Step 5: first resample")
+    println("Step 5: Create a resampledData and compute the mean mpg and variance for each category")
     val firstResample = sampleRdd.sample(true,1).cache()
-    firstResample.show(100)
     var result = firstResample.groupBy(K).agg(avg(V) as "Mean", variance(V) as "Variance").cache()
-    result.show(100)
-    
+    result.show()
+    println("10%")
     for(i<-2 to TIMES){
       var resampledData = sampleRdd.sample(true,1).groupBy(K).agg(avg(V) as "Mean", variance(V) as "Variance")
       result = resampledData.union(result).cache()
+      println((100/TIMES * i) + "%")
     }
 
     println("Step 5: sum mean, variance")
@@ -68,7 +64,7 @@ object CarPrice {
       format_number(sum("Mean")/TIMES,2) as "Mean",
       format_number(sum("Variance")/TIMES,2) as "Variance"
     )
-    
+
     res.show()
   }
 }
